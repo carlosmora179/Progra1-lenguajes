93c799dce2db227d315658f4d98c5a68fe9b69c0
Carlos <carlosmora179@gmail.com>
2019-09-11 10:31:05 -0600

    Creacion del arbol y primer avance en list all nodes

--- Programa.rkt
+++ Programa.rkt
@@ -1,3 +1,14 @@
 #lang racket
 
-;Aca incia el codigo 
+;Aca incia el codigo
+
+(struct nodo(id nombre valor hermano hijo)#:transparent #:mutable)
+
+(struct arbol(raiz)#:transparent #:mutable)
+
+
+(define (list-all-nodes arbolito)
+  (cond [(and(null? (nodo-hijo arbolito))(null? (nodo-hermano arbolito))) "fin"]
+        [])
+
+  )
