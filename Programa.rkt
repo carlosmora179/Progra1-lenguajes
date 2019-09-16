c3a6ba14a07affbf904ee37d1769fecfb70a348d
Carlos <carlosmora179@gmail.com>
2019-09-15 20:39:26 -0600

    Se agragron 2 funciones hermano derecho y hermano izquierdo
    ademas se documento por completo el documento en cada una de sus funciones.

--- Programa.rkt
+++ Programa.rkt
@@ -34,6 +34,7 @@
 ;funcion que imprime el arbol con un recorrido a profundidad de izquierda a derecha
 ;entradas:arbol a imprimir
 ;salidas: lista de los nodos impresos a profundidad.
+;ejecucion:>(list_all_nodes arbol)
 (define (list_all_nodes arbolito)
   (if(arbol? arbolito);verificacion de la estructura
      (remove-duplicates(list_all_nodes_aux (arbol-raiz arbolito)));llamda a la funcion auxiliar de imprimir todo el arbol
@@ -55,6 +56,7 @@
 ;funcion para listar algunos nodos del arbol (ramas)
 ;entradas:arbol y nodo inicial
 ;salidas:lista con los nodos desde el epecificado
+;ejecucion:>(list_some_nodes arbol nodoinicio)
 (define (list_some_nodes arbolito nodoinicio)
   (if(and (arbol? arbolito) (nodo? nodoinicio) );verificacion de la estructura
      (list_some_nodes_aux (first (find_node arbol1 nodoinicio)) 0 );llamda a la funcion auxiliar de imprimir todo el arbol
@@ -81,6 +83,7 @@
 ;funcion para buscar un nodo
 ;entradas: el arbol y el nodo a buscar
 ;salidas: lista con el nodo si existe si no empty
+;ejecucion:>(find_node arbol nodoabuscar)
 (define (find_node arbolito nodo)
   (if(and (arbol? arbolito) (nodo? nodo)) (find_node_aux(arbol-raiz arbolito) nodo);verificacion de valores de entrada
      "valores de entrada incorectos"))
@@ -105,6 +108,7 @@
 ;funcion para insertar un nodo
 ;entradas:arbol nodo padre y el nodo que se vaa insertar
 ;salidas el arbol con el nuevo nodo insertado en su posicion
+;ejecucion:>(insert_node arbol nodopadre nodohijo)
 (define(insert_node arbolito padre nodohijo)
   (if (and(arbol? arbolito)(nodo? padre)(nodo? nodohijo));verificacion de entradas
       (insert_node_aux(first(find_node arbolito padre)) nodohijo )
@@ -136,6 +140,7 @@
 ;funcion para eliminar un nodo
 ;entradas: el arbol y el nodo a eliminar
 ;salidas: el arbol con el nodo y su rama eliminado
+;ejecucion:>(delete_node arbol nodoaeliminar)
 (define (delete_node arbolito nodoeliminar)
 ;verificacion de valores de entrada
   (if(and (arbol? arbolito) (nodo? nodoeliminar))
@@ -154,9 +159,9 @@
 (define (delete_node_aux raiz nodoeliminar ultimovisitado estatus)
   (cond [(empty? raiz);si esta vacio
       empty ];devuelvo esto al estar vacio
-  
+  ;si no esta vacio hago todo esto
   [ (equal? raiz nodoeliminar) (list ultimovisitado estatus )];verifica que sea el nodo buscado y devuelve el ultimo visitado + el estatus
-      ;si no esta vacio hago todo esto
+      
       [(append(delete_node_aux (nodo-hijo raiz) nodoeliminar raiz 0 );aca llamo con el hijo y cambio al ultimo visitado por el actual y el estatus por 0
               (delete_node_aux (nodo-hermano raiz) nodoeliminar raiz 1 ) )]);aca con el hermano cambio al ultimo visitado por el actual y el estatus por 1
 
@@ -168,6 +173,7 @@
 ;funcion para sacar el ancestro de un nodo
 ;entradas: el arbol y el nodo a consultar
 ;salidas: una lista con el nodo ancestro
+;ejecucion:>(ancestor arbol nodoaconsultar)
 (define (ancestor arbolito nodo)
   (if(and (arbol? arbolito) (nodo? nodo)) (ancestor_aux(arbol-raiz arbolito) nodo empty) ;verificacion de valores de entrada
      "valores de entrada incorectos"))
@@ -181,16 +187,48 @@
 
  (cond [(empty? raiz);si esta vacio
       empty ];devuelvo esto al estar vacio
-  
+   ;si no esta vacio hago todo esto
   [ (equal? raiz nodobuscado) (list ultimopadre )];verifica que sea el nodo buscado y devuelve el ultimo padre
-      ;si no esta vacio hago todo esto
+     
       [(append(ancestor_aux (nodo-hijo raiz) nodobuscado raiz );aca llamo con el hijo y cambio al ultimo padre por el actual
               (ancestor_aux (nodo-hermano raiz) nodobuscado ultimopadre ) )]);aca con el hermano ys e mantiene el ultimo padre
 
   )
   
   
+;funcion para buscar el hermano derecho de un nodo
+;entradas: arbol y el nodo a buscar su hermano
+;salidas: el nodo correspondiente al hermano derecho del nodo buscado
+;ejecucion:>(find_right_sibling arbol nodoaconsultar)
+(define(find_right_sibling arbolito nodobuscado )
+  (if(and (arbol? arbolito) (nodo? nodobuscado));verificacion de valores de entrada
+     (nodo-hermano (first(find_node arbolito nodobuscado))) ;llamada a la busqueda del nodo y se devuelve su hermano derecho
+     "valores de entrada incorectos")
+  )
+
+;funcion para buscar el hermano izquierdo de un nodo
+;entradas: arbol y el nodo a buscar su hermano
+;salidas: el nodo correspondiente al hermano izquierdo del nodo buscado
+;ejecucion:>(find_left_sibling arbol nodoaconsultar)
+(define(find_left_sibling arbolito nodobuscado)
+  (if(and (arbol? arbolito) (nodo? nodobuscado)) (find_left_sibling_aux(arbol-raiz arbolito) nodobuscado empty) ;verificacion de valores de entrada
+     "valores de entrada incorectos"))
+
+  
+;funcion auxiliar para encontrar el hermano izquierdo de un nodo
+;cambios: ultimo visitado donde se guarda el hermano izquierdo del nodo
+(define (find_left_sibling_aux raiz nodobuscar ultimovisitado )
+  (cond [(empty? raiz);si esta vacio
+      empty ];devuelvo esto al estar vacio
+   ;si no esta vacio hago todo esto
+  [ (equal? raiz nodobuscar) (list ultimovisitado)];verifica que sea el nodo buscado y devuelve el ultimo visitado
+     
+      [(append(find_left_sibling_aux (nodo-hijo raiz) nodobuscar empty);aca llamo con el hijo y cambio al ultimo visitado por vacio
+              (find_left_sibling_aux (nodo-hermano raiz) nodobuscar raiz ) )]);aca con el hermano cambio al ultimo visitado por el actual
 
 
+  )
+
+  
 
 
